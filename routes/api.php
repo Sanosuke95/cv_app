1c5993aeab910f98f2a49108599a4c0ea9f3c002
sakkisuke <cedric-valcy@hotmail.fr>
2024-05-02 23:19:29 +0200

    Add contact for database and method

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('/contact', ContactController::class)->only(['store', 'index', 'show', 'destroy']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [AuthController::class, 'profile']);
