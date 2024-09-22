c840b31a8c27338b2839f443e3f5d39866b744c2
sakkisuke <cedric-valcy@hotmail.fr>
2024-09-22 11:07:55 +0200

    Ajout de des card pour les formulaires

--- resources/js/components/form/Form.jsx
+++ resources/js/components/form/Form.jsx
@@ -2,7 +2,6 @@
 const Form = ({ children, handleSubmit }) => {
     return (
         <form onSubmit={handleSubmit}>
-            <h3>Premier form</h3>
             <div className="container">{children}</div>
         </form>
     );
--- resources/js/components/layout/Layout.jsx
+++ resources/js/components/layout/Layout.jsx
@@ -1,10 +1,11 @@
 import Nav from "../nav/Nav";
+import "./layout.css";
 
 function Layout({ children }) {
     return (
         <>
             <Nav />
-            <div className="container">
+            <div className="container mt-5">
                 <div>{children}</div>
             </div>
         </>
